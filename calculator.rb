0d26ab8ddd414010cd560ee9f9826da27bd3c8cf
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-28 14:43:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,7 @@ sum = first_number + second_number
 
 difference = first_number - second_number
 
-product = first_number ** second_number
+product = first_number * second_number
 
 
 # Add your variables here
